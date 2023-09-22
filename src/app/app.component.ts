48b60e5e8ce4382aba957529434a9ec055882bdb
DaviGFiguer <davigarciafigueiredo@gmail.com>
2023-09-22 17:19:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CepServiceService } from './cep-service.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CepAngular';
+
+  constructor(private cepService: CepServiceService) {
+
+  }
+
+  consultaCep(valor: any, form: any) {
+    this.cepService.buscar(valor).subscribe((dados) => this.populaForm(dados, form))
+  }
+
+  populaForm(dados: any, form: any) {
+    form.setValue({
+      cep: dados.cep,
+      logradouro: dados.logradouro,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      uf: dados.uf
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    return filterValue;
+  }
 }
